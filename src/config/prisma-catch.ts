cef26b7425fc644fdc61b454a22e9e68dd4d2e53
kevinkeyssx <keyss1700@gmail.com>
2025-03-21 04:10:59 -0300

    Roles services done | Roles now accepts the same name but for usury

--- src/config/prisma-catch.ts
+++ src/config/prisma-catch.ts
@@ -15,16 +15,16 @@ export enum ERROR_MESSAGES {
 export class PrismaException {
     static readonly #logger = new Logger( PrismaException.name );
 
-    static catch( exception: any ) {
+    static catch( exception: any, message?: string ) {
         if (exception.code === ERROR_MESSAGES.ALREADY_EXISTS) {
-            this.#logger.error(`${exception.meta.target} already exists.`);
-            throw new BadRequestException( `${exception.meta.target} already exists.` );
+            this.#logger.error(`${message ?? exception.meta.target} already exists.`);
+            throw new BadRequestException( `${ message ?? exception.meta.target} already exists.` );
         }
 
         if (exception.code === ERROR_MESSAGES.NOT_FOUND) {
             const id = (exception.message as string).split(':')[2].split('_')[0].replace(' `', '');
-            this.#logger.error(`${id} not found.`);
-            throw new BadRequestException( `${id} not found.` );
+            this.#logger.error( `${message ?? id} not found.`);
+            throw new BadRequestException( `${ message ?? id} not found.` );
         }
 
         this.#logger.error( `Error unknown ${ERROR_MESSAGES.UNKNOWN}: ${exception.code}` );
--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,6 +1,4 @@
-import { PrismaException } from '@config/prisma-catch';
 import {
-    BadRequestException,
     ForbiddenException,
     Injectable,
     NotFoundException,
@@ -9,6 +7,7 @@ import {
 
 import { PrismaClient, Role, UserRole } from '@prisma/client';
 
+import { PrismaException }  from '@config/prisma-catch';
 import { CreateRoleInput }  from '@roles/dto/create-role.input';
 import { UpdateRoleInput }  from '@roles/dto/update-role.input';
 import { User }             from '@user/entities/user.entity';
@@ -22,30 +21,20 @@ export class RolesService extends PrismaClient implements OnModuleInit {
     }
 
 
-    async #validName( name: string, currentUser: User ): Promise<void> {
-        const role = await this.role.findUnique({
-            where: {
-                name,
-                userId: currentUser.id
-            }
-        });
-
-        if ( role ) throw new BadRequestException( 'Role already exists' );
-    }
-
-    // TODO: HAY QUE ASIGNAR UN ROLE CUANDO SE CREA UN USUARIO NORMAL
     async create(
         currentUser     : User,
         createRoleInput : CreateRoleInput
     ): Promise<Role> {
-        await this.#validName( createRoleInput.name, currentUser );
-
-        return this.role.create({
-            data: {
-                ...createRoleInput,
-                userId: currentUser.id
-            }
-        });
+        try {
+            return await this.role.create({
+                data: {
+                    ...createRoleInput,
+                    userId: currentUser.id
+                }
+            });
+        } catch (error) {
+            throw PrismaException.catch( error, 'Role' );
+        }
     }
 
 
@@ -54,12 +43,13 @@ export class RolesService extends PrismaClient implements OnModuleInit {
         userId: string,
         currentUser: User
     ): Promise<UserRole> {
-        await this.findOne( roleId, currentUser );
+        // await this.findOne( roleId, currentUser );
 
         const user = await this.user.findUnique({ where: { id: userId }});
 
         if ( !user ) throw new NotFoundException( `User whit id ${userId} not found.` );
-        if ( user.apiUserId !== currentUser.id ) throw new ForbiddenException( 'You are not allowed to assign a role to this user.' );
+        if ( user.apiUserId !== currentUser.id && user.id !== currentUser.id )
+            throw new ForbiddenException( 'You are not allowed to assign a role to this user.' );
 
         try {
             return await this.userRole.create({
@@ -73,7 +63,7 @@ export class RolesService extends PrismaClient implements OnModuleInit {
                 }
             });
         } catch (error) {
-            throw PrismaException.catch( error );
+            throw PrismaException.catch( error, 'Role' );
         }
     }
 
@@ -88,7 +78,7 @@ export class RolesService extends PrismaClient implements OnModuleInit {
     }
 
 
-    async findOne( id: string, currentUser: User ) {
+    async findOne( id: string, currentUser: User ): Promise<Role> {
         // TODO: Add pagination, by UserId, 
         const role = await this.role.findUnique({
             where: {
@@ -98,29 +88,29 @@ export class RolesService extends PrismaClient implements OnModuleInit {
         });
 
         if ( !role ) throw new NotFoundException( `Role whit id ${id} not found` );
-        if ( role.userId !== currentUser.id ) throw new ForbiddenException( 'You are not allowed to access this role.' );
 
         return role;
     }
 
 
-    // TODO: HAY UN BUG Cuando actualizo un name, pero ya existe para otro usuario
-    // TODO: Esto no debería pasar ya que el name debe ser único por usuario id
     async update(
         updateRoleInput: UpdateRoleInput,
         currentUser: User
     ): Promise<Role> {
         await this.findOne( updateRoleInput.id, currentUser );
 
-        if ( updateRoleInput.name ) await this.#validName( updateRoleInput.name, currentUser );
-
-        return this.role.update({
-            where: {
-                id: updateRoleInput.id,
-                userId: currentUser.id
-            },
-            data: updateRoleInput
-        });
+        try {
+            return await this.role.update({
+                where: {
+                    id: updateRoleInput.id,
+                    userId: currentUser.id
+                },
+                data: updateRoleInput
+            });
+        }
+        catch ( error ) {
+            throw PrismaException.catch( error, 'Role' );
+        }
     }
 
 
