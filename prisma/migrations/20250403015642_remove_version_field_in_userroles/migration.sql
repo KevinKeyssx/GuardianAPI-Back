29a9e747d6558e8ff42947f978fdd85649cb2ead
kevinkeyssx <keyss1700@gmail.com>
2025-04-02 22:59:08 -0300

    Remove version field in UserRoles

---
+++ prisma/migrations/20250403015642_remove_version_field_in_userroles/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `version` on the `UserRole` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "UserRole" DROP COLUMN "version";
