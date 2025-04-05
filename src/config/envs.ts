adc6cd9dcb7b1b359b1da47d0acc0ef8a8c8e08c
kevinkeyssx <keyss1700@gmail.com>
2025-04-05 01:31:07 -0300

    IoRedis added

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -14,8 +14,10 @@ interface EnvVars {
     SOCIAL_X_AUTH           : string;
     SOCIAL_TWITCH_AUTH      : string;
     FREE_PLAN_ID            : string;
+    REDIS_HOST              : string;
+    REDIS_PORT              : number;
+    REDIS_PASSWORD          : string;
 }
-
 
 const envsSchema = joi.object({
     PORT                    : joi.number().required(),
@@ -29,9 +31,11 @@ const envsSchema = joi.object({
     SOCIAL_X_AUTH           : joi.string().required(),
     SOCIAL_TWITCH_AUTH      : joi.string().required(),
     FREE_PLAN_ID            : joi.string().required(),
+    REDIS_HOST              : joi.string().required(),
+    REDIS_PORT              : joi.number().required(),
+    REDIS_PASSWORD          : joi.string().required()
 })
 .unknown( true );
-
 
 const { error, value } = envsSchema.validate( process.env );
 
@@ -54,4 +58,7 @@ export const ENVS = {
     SOCIAL_X_AUTH           : envVars.SOCIAL_X_AUTH,
     SOCIAL_TWITCH_AUTH      : envVars.SOCIAL_TWITCH_AUTH,
     FREE_PLAN_ID            : envVars.FREE_PLAN_ID,
+    REDIS_HOST              : envVars.REDIS_HOST,
+    REDIS_PORT              : envVars.REDIS_PORT,
+    REDIS_PASSWORD          : envVars.REDIS_PASSWORD,
 }
