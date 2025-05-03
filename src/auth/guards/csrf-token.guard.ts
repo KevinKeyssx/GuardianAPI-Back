3ccc71dcd0edaa7e8829d4a7c5feb9e5a13f2771
kevinkeyssx <keyss1700@gmail.com>
2025-05-03 02:26:52 -0400

    csrf validation is omitted if you are a customer like Apollo or if we are in a development environment

--- src/auth/guards/csrf-token.guard.ts
+++ src/auth/guards/csrf-token.guard.ts
@@ -1,20 +1,60 @@
-import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
 import { Request } from 'express';
+
+import {
+    Injectable,
+    CanActivate,
+    ExecutionContext,
+    UnauthorizedException,
+    Logger
+} from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
 
 @Injectable()
 export class CsrfGuard implements CanActivate {
+    private readonly logger = new Logger(CsrfGuard.name);
+
     canActivate(context: ExecutionContext): boolean {
-        const request: Request = context.switchToHttp().getRequest();
-        console.log('🚀 ~ file: csrf-token.guard.ts:8 ~ request:', request)
-        if (!request.url.includes('/graphql')) return true; // Solo para GraphQL
+        let request: Request;
+
+        if ( context.getType<string>() === 'graphql' ) {
+            const gqlContext = GqlExecutionContext.create( context );
+            const ctx = gqlContext.getContext();
+            request = ctx.req;
+        } else {
+            request = context.switchToHttp().getRequest<Request>();
+        }
+
+        if ( !request ) {
+            this.logger.error('Request object is undefined');
+            throw new UnauthorizedException( 'Unable to process request' );
+        }
+
+        this.logger.debug(`Checking CSRF for URL: ${request.url || 'unknown'}, Origin: ${request.headers.origin || 'undefined'}`);
+
+        if ( !request.url.includes('/graphql' )) {
+            this.logger.debug( 'Non-GraphQL request, skipping CSRF validation' );
+            return true;
+        }
+
+        const isDevelopment = process.env.NODE_ENV === 'development';
+        const bypassCsrf    = request.headers['x-apollo-bypass-csrf'] === 'true';
+
+        if ( isDevelopment && bypassCsrf ) {
+            this.logger.debug( 'CSRF validation bypassed in development with X-Apollo-Bypass-CSRF' );
+            return true;
+        }
 
         const csrfToken = request.headers['x-csrf-token'] as string;
         const storedCsrfToken = request.cookies?.csrfToken;
 
-        if (!csrfToken || csrfToken !== storedCsrfToken) {
-            throw new UnauthorizedException('Invalid CSRF token');
+        this.logger.debug( `CSRF Token: ${csrfToken}, Stored CSRF Token: ${storedCsrfToken}` );
+
+        if ( !csrfToken || csrfToken !== storedCsrfToken ) {
+            this.logger.error( 'Invalid CSRF token' );
+            throw new UnauthorizedException( 'Invalid CSRF token' );
         }
 
+        this.logger.debug( 'CSRF validation passed' );
         return true;
     }
 }
