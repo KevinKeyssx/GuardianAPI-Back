1f7151e24811044ddc28f5b45f300d71ba4b42b6
kevinkeyssx <keyss1700@gmail.com>
2025-06-01 22:41:06 -0400

    Update User Attribute value input deleted

--- src/user-attribute-values/dto/update-user-attribute-value.input.ts
+++
@@ -1,8 +0,0 @@
-import { CreateUserAttributeValueInput } from './create-user-attribute-value.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
-
-@InputType()
-export class UpdateUserAttributeValueInput extends PartialType(CreateUserAttributeValueInput) {
-  @Field(() => Int)
-  id: number;
-}
