0ceae367eb7f480f7a0435626d0b8a5077801304
kevinkeyssx <keyss1700@gmail.com>
2025-06-14 23:53:48 -0400

    Name added

--- src/secrets/dto/create-secret.input.ts
+++ src/secrets/dto/create-secret.input.ts
@@ -1,9 +1,13 @@
 import { InputType, Field } from '@nestjs/graphql';
 
-import { IsDate, IsOptional } from 'class-validator';
+import { IsDate, IsOptional, IsString } from 'class-validator';
 
 @InputType()
 export class CreateSecretInput {
+
+    @IsString()
+    @Field( () => String )
+    name: string;
 
     @IsDate()
     @IsOptional()
