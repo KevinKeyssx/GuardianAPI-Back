d98bd97abc037d0959bd763827ed687ca956e1bd
kevinkeyssx <keyss1700@gmail.com>
2025-07-05 22:59:03 -0400

    UpdateUserInput updated

--- src/user/dto/update-user.input.ts
+++ src/user/dto/update-user.input.ts
@@ -1,83 +1,16 @@
 import { Field, ID, InputType } from '@nestjs/graphql';
+import { PartialType }          from '@nestjs/swagger';
 
-import { IsDate, IsEmail, IsOptional, IsString, IsUUID, Length } from 'class-validator';
+import { IsUUID } from 'class-validator';
 
-import { Type } from 'class-transformer';
+import { CreateUserInput } from './create-user.input';
 
 
 @InputType()
-export class UpdateUserInput {
+export class UpdateUserInput extends PartialType( CreateUserInput ) {
 
     @Field( () => ID )
     @IsUUID()
     id: string;
-
-    @Field( () => String, { nullable: true })
-    @IsEmail()
-    @Length( 1, 80 )
-    @IsOptional()
-    email?: string;
-
-    @Field(() => String, { nullable: true })
-    @IsOptional()
-    @IsString()
-    @Length( 3, 30 )
-    nickname?: string;
-
-    @Field(() => String, { nullable: true })
-    @IsOptional()
-    @IsString()
-    @Length(1, 100)
-    name?: string;
-
-    @Field(() => String, { nullable: true })
-    @IsOptional()
-    @IsString()
-    phone?: string;
-
-    @Field(() => Date, { nullable: true })
-    @IsOptional()
-    @Type(() => Date)
-    @IsDate()
-    birthdate?: Date;
-
-    @Field(() => String, { nullable: true })
-    @IsOptional()
-    @IsString()
-    avatar?: string;
-
-    @Field( () => String, { nullable: true })
-    @IsOptional()
-    @IsString()
-    googleId?: string;
-
-    @Field( () => String, { nullable: true })
-    @IsOptional()
-    @IsString()
-    microsoftId?: string;
-
-    @Field( () => String, { nullable: true })
-    @IsOptional()
-    @IsString()
-    facebookId?: string;
-
-    @Field( () => String, { nullable: true })
-    @IsOptional()
-    @IsString()
-    githubId?: string;
-
-    @Field( () => String, { nullable: true })
-    @IsOptional()
-    @IsString()
-    appleId?: string;
-
-    @Field( () => String, { nullable: true })
-    @IsOptional()
-    @IsString()
-    otherId?: string;
-
-    @Field(() => Boolean, { nullable: true })
-    @IsOptional()
-    isActive?: boolean;
 
 }
