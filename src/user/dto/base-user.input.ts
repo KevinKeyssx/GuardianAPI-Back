a35f833a145448c58f9458926263973d6f734ed5
kevinkeyssx <keyss1700@gmail.com>
2025-07-19 23:14:21 -0400

    BaseUserInput added

---
+++ src/user/dto/base-user.input.ts
@@ -0,0 +1,54 @@
+import { Field, ID, InputType } from '@nestjs/graphql';
+
+import {
+    IsBoolean,
+    IsDate,
+    IsOptional,
+    IsString,
+    IsUUID,
+    Length
+}               from 'class-validator';
+import { Type } from 'class-transformer';
+
+@InputType()
+export class BaseUserInput {
+
+    @Field(() => String, { nullable: true })
+    @IsOptional()
+    @IsString()
+    @Length( 3, 30 )
+    nickname?: string;
+
+    @Field(() => String, { nullable: true })
+    @IsOptional()
+    @IsString()
+    @Length(1, 100)
+    name?: string;
+
+    @Field(() => String, { nullable: true })
+    @IsOptional()
+    @IsString()
+    phone?: string;
+
+    @Field(() => Date, { nullable: true })
+    @IsOptional()
+    @Type(() => Date)
+    @IsDate()
+    birthdate?: Date;
+
+    @Field(() => ID, { nullable: true })
+    @IsOptional()
+    @IsUUID()
+    apiUserId?: string;
+
+    @Field(() => Boolean, { nullable: true })
+    @IsOptional()
+    @IsBoolean()
+    isActive: boolean = true;
+
+    @Field(() => Boolean, { nullable: true })
+    @IsOptional()
+    @IsBoolean()
+    isVerified: boolean = false;
+
+}
