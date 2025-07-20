b94da362b1d89e8233c2246bc220acd7852c6bba
kevinkeyssx <keyss1700@gmail.com>
2025-07-19 23:14:31 -0400

    BaseUserInput implemented

--- src/user/dto/create-user.input.ts
+++ src/user/dto/create-user.input.ts
@@ -1,61 +1,16 @@
-import { Field, ID, InputType } from '@nestjs/graphql';
+import { Field, InputType } from '@nestjs/graphql';
 
-import {
-    IsBoolean,
-    IsDate,
-    IsEmail,
-    IsOptional,
-    IsString,
-    IsUUID,
-    Length
-}               from 'class-validator';
-import { Type } from 'class-transformer';
+import { IsEmail, Length } from 'class-validator';
+
+import { BaseUserInput } from '@user/dto/base-user.input';
 
 
 @InputType()
-export class CreateUserInput {
+export class CreateUserInput extends BaseUserInput {
 
     @Field( () => String )
     @IsEmail()
     @Length( 1, 80 )
     email: string;
-
-    @Field(() => String, { nullable: true })
-    @IsOptional()
-    @IsString()
-    @Length( 3, 30 )
-    nickname?: string;
-
-    @Field(() => String, { nullable: true })
-    @IsOptional()
-    @IsString()
-    @Length(1, 100)
-    name?: string;
-
-    @Field(() => String, { nullable: true })
-    @IsOptional()
-    @IsString()
-    phone?: string;
-
-    @Field(() => Date, { nullable: true })
-    @IsOptional()
-    @Type(() => Date)
-    @IsDate()
-    birthdate?: Date;
-
-    @Field(() => ID, { nullable: true })
-    @IsOptional()
-    @IsUUID()
-    apiUserId?: string;
-
-    @Field(() => Boolean, { nullable: true })
-    @IsOptional()
-    @IsBoolean()
-    isActive: boolean = true;
-
-    @Field(() => Boolean, { nullable: true })
-    @IsOptional()
-    @IsBoolean()
-    isVerified: boolean = false;
 
 }
